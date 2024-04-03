6465977a71f31495874fb5d96649e5983bc6c31c
Kostia S <kosiak5587@gmail.com>
2024-04-03 12:33:18 +0600

    feat: add cookie alert

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import controlTabsHeader from './components/controlTabsHeader.js';
 import controlMenuMobile from './components/controlMenuMobile.js';
+import controlCookieAlert from './components/controlCookieAlert.js';
 
-// controlTabsHeader();
-// controlMenuMobile();
+controlTabsHeader();
+controlMenuMobile();
+controlCookieAlert();
